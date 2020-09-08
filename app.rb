cf4657c2090e860eb9350dce5e9d6a94b96d4793
edmundo papi <edmundopierre@yahoo.com>
2020-09-08 08:55:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,11 +13,11 @@ class App < Sinatra::Base
     "Hello #{@user_name}!"
   end
 
-#   # Code your final two routes here:
-# get "/goobye/:name" do
-#   @user_name = params[:name]
-#   "Goodbye, #{@user_name}."
-# end
+  # Code your final two routes here:
+get "/goobye/:name" do
+  @user_name = params[:name]
+  "Goodbye, #{@user_name}."
+end
 # get '/multiply/num1/num2' do
 #    @num1 = params[:num1]
 #    @num2 = params[:num2]
