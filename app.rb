a5118e0879c8a24d802cac12e22c89d90764e594
edmundo papi <edmundopierre@yahoo.com>
2020-09-08 08:36:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,9 +18,9 @@ get '/goobye/:name' do
   @user_name = params[:name]
   "Goodbye, #{@user_name}."
 end
-get '/multiply/num1/num2' do
-   @num1 = params[:num1]
-   @num2 = params[:num2]
-   "#{@num1 * @num2}"
-end
+# get '/multiply/num1/num2' do
+#    @num1 = params[:num1]
+#    @num2 = params[:num2]
+#    "#{@num1 * @num2}"
+# end
 end
